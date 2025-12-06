73868750f05b368ac3526dd5a43cb3b5a41d385e
tahmidur22 <tahmidur55@gmail.com>
2025-12-06 17:46:09 +0000

    testing kubernetes public

--- modules/aks/main.tf
+++ modules/aks/main.tf
@@ -3,7 +3,7 @@ resource "azurerm_kubernetes_cluster" "aks" {
   location                = var.location
   resource_group_name     = var.resource_group_name
   dns_prefix              = var.dns_prefix
-  private_cluster_enabled = true
+  private_cluster_enabled = false
   kubernetes_version      = var.kubernetes_version
 
   default_node_pool {
